04937a320aad87648243b32afd5d08c25a6984c5
SauravKanchan <sauravnk30@gmail.com>
2018-09-21 17:10:08 +0530

    Add .bna

--- dvoting/lib/logic.js
+++ dvoting/lib/logic.js
@@ -1,43 +1,33 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
 /**
- * Write your transction processor functions here
- */
-
-/**
- * Sample transaction
- * @param {org.party.SampleTransaction} sampleTransaction
+ * Sample transaction processor function.
+ * @param {org.party.Vote} tx The sample transaction instance.
  * @transaction
  */
-async function sampleTransaction(tx) {
-    // Save the old value of the asset.
-    const oldValue = tx.asset.value;
+async function Vote(tx) {  // eslint-disable-line no-unused-vars
 
-    // Update the asset with the new value.
-    tx.asset.value = tx.newValue;
+
+  	if (tx.voter.voted==0){
+      if (tx.newValue==0){
+          tx.asset.bjp = tx.asset.bjp+1;
+      }else if(tx.newValue==1){
+          tx.asset.cong = tx.asset.cong+1
+      }else if(tx.newValue==2){
+          tx.asset.aap = tx.asset.aap+1
+      }else if(tx.newValue==3){
+          tx.asset.ncp = tx.asset.ncp+1
+      }
+    }
+	tx.voter.voted = 1
 
     // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('org.party.SampleAsset');
+    const assetRegistry = await getAssetRegistry('org.party.Result');
     // Update the asset in the asset registry.
     await assetRegistry.update(tx.asset);
 
-    // Emit an event for the modified asset.
-    let event = getFactory().newEvent('org.party', 'SampleEvent');
-    event.asset = tx.asset;
-    event.oldValue = oldValue;
-    event.newValue = tx.newValue;
-    emit(event);
+  	    // Get the asset registry for the asset.
+    const voterRegistry = await getParticipantRegistry('org.party.Voter');
+    // Update the asset in the asset registry.
+    await voterRegistry.update(tx.voter);
+
+
 }
